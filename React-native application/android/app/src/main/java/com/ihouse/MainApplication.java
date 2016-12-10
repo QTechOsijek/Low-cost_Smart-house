12359b83d2ed2c422f9e5507dc219ba60e6271fd
DESKTOP-UNBNLOR\filip <filip.pilot@gmail.com>
2016-12-10 18:20:19 +0100

    Android ne baca error

--- React-native application/android/app/src/main/java/com/ihouse/MainApplication.java
+++ React-native application/android/app/src/main/java/com/ihouse/MainApplication.java
@@ -27,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics)),
             new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics))
       );
     }
